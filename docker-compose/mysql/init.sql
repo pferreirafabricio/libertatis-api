f12f53727180fb37079517287edb4f7a203ccc13
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-05-01 18:31:31 -0300

    feat(mysql): add init.sql

--- docker-compose/mysql/init.sql
+++ docker-compose/mysql/init.sql
@@ -1,9 +1,11 @@
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `players`;
 
-CREATE TABLE `users` (
-  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `players` (
+  `nick` varchar(60) NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`nick`)
 );
 
-INSERT INTO `users` (name) VALUES ('admin'),('mr. robot');
+INSERT INTO players(nick, name) VALUES('admin', 'Administrator');
--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -5,7 +5,7 @@
  */
 
 define('CONF_API_ALLOW_ORIGIN', '*');
-define('CONF_API_CONTENT_TYPE', 'application/json');
+define('CONF_API_CONTENT_TYPE', 'application/json, text/html');
 define('CONF_API_CHARSET', 'utf-8');
 define('CONF_API_ALLOW_METHODS', ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]);
 define('CONF_API_MAX_AGE', '3600');
